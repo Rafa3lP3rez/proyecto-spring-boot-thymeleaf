54e3fa713d5d30fefa435488fef8973631ede557
= <=>
2021-11-22 22:27:52 -0600

    Creando el controller y el pojo

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,4 +1,23 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
 public class HomeController {
+
+
+
+    @GetMapping("/list")
+    public String getEmployees(){
+
+        return "list-employees";
+    }
+
+
+
 }
