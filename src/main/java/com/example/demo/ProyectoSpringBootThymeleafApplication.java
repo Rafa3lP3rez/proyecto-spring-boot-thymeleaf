2677378170f9ff1d922d14ab882088cfb4b52056
= <=>
2021-11-22 22:20:02 -0600

    Initial commit

---
+++ src/main/java/com/example/demo/ProyectoSpringBootThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSpringBootThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSpringBootThymeleafApplication.class, args);
+	}
+
+}
